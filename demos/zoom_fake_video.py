4dca78d33db964d5d15a9d4952e9bfa5c27c95cf
GeneralCoder365 <ainesh.chatterjee@gmail.com>
2021-08-23 15:48:38 -0400

    dgsfdg

--- demos/zoom_fake_video.py
+++ demos/zoom_fake_video.py
@@ -3,3 +3,23 @@ import jwt
 import requests
 import json
 from time import time
+
+API_KEY = "M6_R9yGdSbanSS7lYMUKJA"
+API_SECRET = "f9hS05Rz4N94F6Ar3D0hFOwQk0xN5syPGaDK"
+
+# create a function to generate a token 
+# using the pyjwt library
+def generateToken():
+    token = jwt.encode(
+        
+        # Create a payload of the token containing:
+        # API Key & expiration time
+        {'iss': API_KEY, 'exp': time() + 5000},
+          
+        # Secret used to generate token signature
+        API_SECRET,
+
+        # Specify the hashing algorithm
+        algorithm = 'HS256'
+    )
+    return token
